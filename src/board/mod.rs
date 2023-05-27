76fcaef6e0539acf10da833335484bd1ab62f9da
Yuttana Suputthayangkun <yuttana.suputthayangkun@gmail.com>
2023-05-27 19:11:44 +0700

    move size module

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -1,42 +1,13 @@
 mod builder;
+mod size;
 
 pub use builder::*;
+pub use size::*;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Axis {
     X,
     Y,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct Size {
-    x: usize,
-    y: usize,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub enum SizeError {
-    ZeroSize(Axis),
-}
-
-pub type SizeResult = Result<Size, SizeError>;
-
-impl Size {
-    pub fn new(x: usize, y: usize) -> SizeResult {
-        match (x, y) {
-            (0, _) => Err(SizeError::ZeroSize(Axis::X)),
-            (_, 0) => Err(SizeError::ZeroSize(Axis::Y)),
-            (x, y) => Ok(Size { x, y }),
-        }
-    }
-}
-
-impl TryFrom<(usize, usize)> for Size {
-    type Error = SizeError;
-
-    fn try_from(value: (usize, usize)) -> Result<Self, Self::Error> {
-        Size::new(value.0, value.1)
-    }
 }
 
 #[derive(Debug, PartialEq, Eq)]
---
+++ src/board/size.rs
@@ -0,0 +1,32 @@
+use super::Axis;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Size {
+    x: usize,
+    y: usize,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum SizeError {
+    ZeroSize(Axis),
+}
+
+pub type SizeResult = Result<Size, SizeError>;
+
+impl Size {
+    pub fn new(x: usize, y: usize) -> SizeResult {
+        match (x, y) {
+            (0, _) => Err(SizeError::ZeroSize(Axis::X)),
+            (_, 0) => Err(SizeError::ZeroSize(Axis::Y)),
+            (x, y) => Ok(Size { x, y }),
+        }
+    }
+}
+
+impl TryFrom<(usize, usize)> for Size {
+    type Error = SizeError;
+
+    fn try_from(value: (usize, usize)) -> Result<Self, Self::Error> {
+        Size::new(value.0, value.1)
+    }
+}
