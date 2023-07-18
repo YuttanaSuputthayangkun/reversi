47066e770a21bdde11dea2388e08387f9f442697
Yuttana Suputthayangkun <yuttana.suputthayangkun@gmail.com>
2023-07-18 22:25:22 +0700

    add test for event system

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ const CELL_COLOR_HOVERED: Color = Color::Rgba {
 
 #[cfg(test)]
 mod bevy_test {
-    use bevy::prelude::{Event, ResMut, Resource, Schedule, Schedules, Update, World};
+    use bevy::prelude::{Event, EventReader, Events, First, ResMut, Resource, Schedule, World};
 
     #[derive(Event)]
     struct Number(i32);
@@ -46,6 +46,40 @@ mod bevy_test {
         assert_eq!(1, world.get_resource::<MyResource>().unwrap().0);
         schedule.run(&mut world);
         assert_eq!(2, world.get_resource::<MyResource>().unwrap().0);
+    }
+
+    #[test]
+    fn read_event_twice() {
+        #[derive(Resource)]
+        struct Counter(i32);
+        #[derive(Event)]
+        struct Event;
+        let mut world = World::new();
+        world.init_resource::<Events<Event>>();
+        world.insert_resource(Counter(0));
+        let mut schedule = Schedule::new();
+        fn read_event(mut event_reader: EventReader<Event>, mut res: ResMut<Counter>) {
+            for _ in event_reader.iter() {
+                res.0 += 1;
+            }
+        }
+        schedule
+            .add_systems(Events::<Event>::update_system)
+            .add_systems(read_event);
+
+        // send first
+        world.send_event(Event);
+        schedule.run(&mut world);
+        assert_eq!(1, world.get_resource::<Counter>().unwrap().0);
+
+        // run again without sending
+        schedule.run(&mut world);
+        assert_eq!(1, world.get_resource::<Counter>().unwrap().0);
+
+        // send second
+        world.send_event(Event);
+        schedule.run(&mut world);
+        assert_eq!(2, world.get_resource::<Counter>().unwrap().0);
     }
 }
 
