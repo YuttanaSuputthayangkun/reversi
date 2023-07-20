d4cf12cd45820c55d14fe6c59b14d09e1aafe1fa
Yuttana Suputthayangkun <yuttana.suputthayangkun@gmail.com>
2023-07-20 22:17:53 +0700

    modularize result module

--- src/game_state/result.rs
+++ src/game_state/result.rs
@@ -1,35 +1,49 @@
 use bevy::prelude::*;
 
+pub use plugin::ResultPlugin;
+
 use super::GameState;
 
-pub struct ResultPlugin;
+mod plugin {
+    use super::*;
 
-#[derive(Clone, Copy)]
-pub struct ResultData {
-    // add score here
-}
+    pub struct ResultPlugin;
 
-#[derive(Event)]
-pub struct ResultEvent(ResultData);
-
-impl Plugin for ResultPlugin {
-    fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_event::<ResultEvent>()
-            .add_systems(OnEnter(GameState::Result), show_result_screen)
-            .add_systems(
-                Update,
-                proceed_button_click.run_if(in_state(GameState::Result)),
-            )
-            .add_systems(OnExit(GameState::Result), clear_result_screen);
+    impl Plugin for ResultPlugin {
+        fn build(&self, app: &mut bevy::prelude::App) {
+            app.add_event::<event::ResultEvent>()
+                .add_systems(OnEnter(GameState::Result), system::show_result_screen)
+                .add_systems(
+                    Update,
+                    system::proceed_button_click.run_if(in_state(GameState::Result)),
+                )
+                .add_systems(OnExit(GameState::Result), system::clear_result_screen);
+        }
     }
 }
 
-fn show_result_screen(mut event_reader: EventReader<ResultEvent>) {
-    for _event in event_reader.iter() {
-        // setup here
+mod event {
+    use super::*;
+
+    #[derive(Clone, Copy)]
+    pub struct ResultData {
+        // add score here
     }
+
+    #[derive(Event)]
+    pub struct ResultEvent(ResultData);
 }
 
-fn clear_result_screen() {}
+mod system {
+    use super::*;
 
-fn proceed_button_click() {}
+    pub fn show_result_screen(mut event_reader: EventReader<event::ResultEvent>) {
+        for _event in event_reader.iter() {
+            // setup here
+        }
+    }
+
+    pub fn clear_result_screen() {}
+
+    pub fn proceed_button_click() {}
+}
