aa30eb946bbfec428fe7055f4b398d75d8997623
Yuttana Suputthayangkun <yuttana.suputthayangkun@gmail.com>
2023-07-20 08:56:46 +0700

    move debug module to its own file

---
+++ src/game_state/debug.rs
@@ -0,0 +1,34 @@
+use super::*;
+use bevy::prelude::*;
+
+const DEBUG_KEYCODE: KeyCode = KeyCode::P;
+
+#[derive(Default)]
+struct DebugGameState;
+
+impl DebugGameState {
+    fn next(&self, game_state: &GameState) -> GameState {
+        match game_state {
+            GameState::Game => GameState::Result,
+            GameState::Result => GameState::Game,
+        }
+    }
+}
+
+pub(super) fn add_debug(app: &mut App) {
+    app.add_systems(Startup, || info!("Debug mode enabled."))
+        .add_systems(Update, debug::next_debug_state_on_keyboard_press);
+}
+
+fn next_debug_state_on_keyboard_press(
+    keyboard_input: Res<Input<KeyCode>>,
+    current_game_state: Res<State<GameState>>,
+    mut next_game_state: ResMut<NextState<GameState>>,
+    debug_game_state: Local<DebugGameState>,
+) {
+    if keyboard_input.just_pressed(DEBUG_KEYCODE) {
+        info!("debug keypress detected");
+        let next_debug_state = debug_game_state.next(current_game_state.get());
+        next_game_state.set(next_debug_state);
+    }
+}
--- src/game_state/mod.rs
+++ src/game_state/mod.rs
@@ -3,6 +3,9 @@ use bevy::prelude::{Plugin, States};
 mod game;
 mod result;
 
+#[cfg(feature = "debug")]
+mod debug;
+
 use game::GamePlugin;
 
 pub mod plugin {
@@ -41,42 +44,4 @@ fn position_pairs<Size: Into<usize> + Copy>(
         .map(|y| (0..board_size_x.into()).map(move |x| (x, y)))
         .flat_map(|x| x)
         .collect()
-}
-
-#[cfg(feature = "debug")]
-mod debug {
-    use super::*;
-    use bevy::prelude::*;
-
-    const DEBUG_KEYCODE: KeyCode = KeyCode::P;
-
-    #[derive(Default)]
-    struct DebugGameState;
-
-    impl DebugGameState {
-        fn next(&self, game_state: &GameState) -> GameState {
-            match game_state {
-                GameState::Game => GameState::Result,
-                GameState::Result => GameState::Game,
-            }
-        }
-    }
-
-    pub(super) fn add_debug(app: &mut App) {
-        app.add_systems(Startup, || info!("Debug mode enabled."))
-            .add_systems(Update, debug::next_debug_state_on_keyboard_press);
-    }
-
-    fn next_debug_state_on_keyboard_press(
-        keyboard_input: Res<Input<KeyCode>>,
-        current_game_state: Res<State<GameState>>,
-        mut next_game_state: ResMut<NextState<GameState>>,
-        debug_game_state: Local<DebugGameState>,
-    ) {
-        if keyboard_input.just_pressed(DEBUG_KEYCODE) {
-            info!("debug keypress detected");
-            let next_debug_state = debug_game_state.next(current_game_state.get());
-            next_game_state.set(next_debug_state);
-        }
-    }
 }
